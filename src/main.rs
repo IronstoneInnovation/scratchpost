95f12558bc0cee4055c73e603ef513d2a520f24d
Tim Adams <adamstimb@gmail.com>
2025-05-26 08:47:09 +0200

    ok

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,25 @@ extern crate queues;
 
 use queues::*;
 
+const MAX_ITEMS: usize = 10;
+
 struct ExpirationQueue {
     q: Queue<String>,
 }
 
 impl ExpirationQueue {
 
-    fn push(&mut self, item: String) {
+    fn push(&mut self, item: String) -> String {
         self.q.add(item).expect("An unexpected error occurred: Couldn't push item to empty Expiration Queue");
         
-        if self.q.size() > 10 {
-            self.q.remove().expect("An unexpected error occurred: Tried to remove item from empty Expiration Queue");
+        if self.q.size() > MAX_ITEMS {
+            let expired_item_key = self.q.remove().expect("An unexpected error occurred: Tried to remove item from empty Expiration Queue");
+            expired_item_key
         } else {
-            0;
+            "".to_string()
         }
     }
+
 }
 
 fn main() {
