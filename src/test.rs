69c70d0e830f76b436060d210646613271dfef63
Tim Adams <adamstimb@gmail.com>
2025-05-26 14:03:50 +0200

    adding tests

---
+++ src/test.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let mut response = client.get(uri!(super::hello)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
+}
